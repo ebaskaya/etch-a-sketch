e74459630c4f49aabaedc6b049eb2951a1e655e5
ebaskaya <egemen.baskaya@outlook.com>
2022-11-23 20:17:57 +0300

    Add button that prompts for canvas size

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
 let canvasSize = 16;
 const canvasContainer = document.querySelector('.canvas-container');
+const sizeSelector = document.querySelector('.size-select');
+
+sizeSelector.addEventListener('click', getSize);
+
+
 const canvasElements = [];
 
+function getSize(){
+    prompt('Enter size(Max: 100): ');
+}
 
 function createGrid(size){
     for(let i= 0; i < size*size; i++){

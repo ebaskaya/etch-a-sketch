850bf44e0dcf71465050dc91e76af1b69fb699a6
ebaskaya <egemen.baskaya@outlook.com>
2022-11-23 19:26:00 +0300

    Put grid creator in function

--- src/script.js
+++ src/script.js
@@ -2,11 +2,17 @@ let canvasSize = 16;
 const canvasContainer = document.querySelector('.canvas-container');
 const canvasElements = [];
 
-for(let i= 0; i < 16; i++){
-    for(let j=0; j<16; j++){
-        const canvasElement = document.createElement('div');
-        canvasElement.style = `background-color: red; border: 1px solid`;
-        canvasElements.push(canvasElement);
-        canvasContainer.appendChild(canvasElement);
+
+function createGrid(size){
+    for(let i= 0; i < size; i++){
+        for(let j=0; j<size; j++){
+            const canvasElement = document.createElement('div');
+            canvasElement.style = `border: 1px solid`;
+            canvasElements.push(canvasElement);
+            canvasContainer.appendChild(canvasElement);
+        }
     }
 }
+
+createGrid(canvasSize);
+

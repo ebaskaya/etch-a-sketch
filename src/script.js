dbcf1d3079e6298c246447b5f16ed2d4af77d6bb
ebaskaya <egemen.baskaya@outlook.com>
2022-11-25 19:29:23 +0300

    Add size selector

--- src/script.js
+++ src/script.js
@@ -1,26 +1,39 @@
 let canvasSize = 16;
 const canvasContainer = document.querySelector('.canvas-container');
 const sizeSelector = document.querySelector('.size-select');
+const MAX_SIZE = 600;
+
+
 
 sizeSelector.addEventListener('click', getSize);
 
 
-const canvasElements = [];
+
 
 function getSize(){
-    prompt('Enter size(Max: 100): ');
+    canvasContainer.replaceChildren();
+    let size = parseInt(prompt('Enter size(Max: 100): '));
+    createGrid(size);
 }
 
 function createGrid(size){
+    setSize(size);
+
+    
     for(let i= 0; i < size*size; i++){
         const canvasElement = document.createElement('div');
         canvasElement.style = `border: 1px solid`;
         canvasElement.classList.add('canvas-element');
         canvasElement.addEventListener('mouseover', fill);
-        canvasElements.push(canvasElement);
+        
         canvasContainer.appendChild(canvasElement);            
         
     }
+}
+
+function setSize(size) {
+    canvasContainer.style.setProperty('--size', size);
+    canvasContainer.style.setProperty('--element-size', (MAX_SIZE/size) + "px");
 }
 
 function fill() {

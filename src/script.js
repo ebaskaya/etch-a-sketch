81b9636b7b1e175a1b470eec032dc341eca57a98
ebaskaya <egemen.baskaya@outlook.com>
2022-11-26 13:36:36 +0300

    Add ability to draw with click and move

--- src/script.js
+++ src/script.js
@@ -11,12 +11,12 @@ sizeSelector.addEventListener('click', getSize);
 
 
 function getSize(){
-    canvasContainer.replaceChildren();
     let size = parseInt(prompt('Enter size(Max: 100): '));
     if(size > 100 || size < 0 || isNaN(size)){
         alert('Invalid entry.');
         return;
     }
+    canvasContainer.replaceChildren();
     createGrid(size);
 }
 
@@ -28,7 +28,10 @@ function createGrid(size){
         const canvasElement = document.createElement('div');
         canvasElement.style = `border: 1px solid`;
         canvasElement.classList.add('canvas-element');
-        canvasElement.addEventListener('mouseover', fill);
+        canvasElement.addEventListener('mousemove', function(e){
+            fill(e);
+        });
+        canvasElement.addEventListener('mousedown', fill);
         
         canvasContainer.appendChild(canvasElement);            
         
@@ -40,8 +43,15 @@ function setSize(size) {
     canvasContainer.style.setProperty('--element-size', (MAX_SIZE/size) + "px");
 }
 
-function fill() {
-    this.style = 'background-color: black';
+function fill(e) {
+    if(e.buttons == 1){
+        e.target.style = 'background-color: black';
+        e.preventDefault();
+        
+        console.log('cizdik');
+        
+    }
+    
 }
 
 createGrid(canvasSize);
